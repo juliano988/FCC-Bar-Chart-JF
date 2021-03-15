fd168af5b214b42a1400e68820d62116431ebe0f
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-03-15 20:03:40 -0300

    Eixo X adicionado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/App.module.css'
 import * as d3 from "d3";
+import * as axis from "d3-axis";
 import { DetailedHTMLProps, HTMLAttributes, LegacyRef, MutableRefObject, RefObject, useEffect, useRef } from 'react';
 
 export default function App({ data }): JSX.Element {
@@ -12,7 +13,7 @@ export default function App({ data }): JSX.Element {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1 id='title'>United States GDP</h1>
-        <Graphic data={data} height='80vh' width='80vw' />
+        <Graphic data={data} height='75vh' width='75vw' />
     </div>
   )
 }
@@ -29,13 +30,18 @@ function Graphic(props:{data:any , height:string , width:string}) {
     const rectBaseWidth = 5;
     const spaceBetweenRect = 1;
 
+    const maxY = Math.max(...props.data.data.map(function(val){return val[1]}))
+    const minY = Math.min(...props.data.data.map(function(val){return val[1]}))
+    const maxX = new Date(props.data.data[props.data.data.length - 1][0])
+    const minX = new Date(props.data.data[0][0])
+
     const scaleX = d3.scaleLinear().domain([0, props.data.data.length * rectBaseWidth + props.data.data.length * spaceBetweenRect]).range([0, svgWidth]);
-    const scaleY = d3.scaleLinear().domain([0, Math.max(...props.data.data.map(function (val) { return val[1] }))]).range([0, svgHeight]);
+    const scaleY = d3.scaleLinear().domain([0, maxY]).range([0, svgHeight]);
 
     const svg = d3.select(divRef.current)
       .append('svg')
-      .attr('width', svgWidth)
-      .attr('height', svgHeight)
+      .attr('width', svgWidth + 20)
+      .attr('height', svgHeight + 20)
 
     svg.selectAll('rect')
       .data(props.data.data)
@@ -45,6 +51,11 @@ function Graphic(props:{data:any , height:string , width:string}) {
       .style('fill', 'blue')
       .attr('x', (d, i) => i * scaleX(rectBaseWidth + spaceBetweenRect))
       .attr('y', (d) => svgHeight - scaleY(d[1]))
+
+    svg.append("g")
+       .attr("transform", "translate(0,"+svgHeight+")")
+       .call(d3.axisBottom(d3.scaleTime().domain([minX,maxX]).range([0,svgWidth])));
+
   }, [])
 
   return (
